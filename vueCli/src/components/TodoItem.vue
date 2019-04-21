fea5f2f0a8d5e6834b734226b3397445ce7a0522
honeysss <907641898@qq.com>
2019-04-21 12:27:38 +0800

    second

---
+++ vueCli/src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <!-- 3.在标签中可以直接使用接收到的值 -->
+  <li @click="deleteHandle(index)">{{content}}</li>
+</template>
+
+<script>
+export default {
+  // 2.子组件用props方法来接收父组件传过来的值
+  props: ['content', 'index'],
+  methods: {
+    deleteHandle (index) {
+      this.$emit('delete', index)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
